541aeb0edb80237fef6732bc870ede44d7e6f658
lgrosse <logan.grosse@learner.42.tech>
2026-07-12 20:58:49 +0200

    refactor: replace list and tuple with numpy array for convenience and performance

--- Pac.py
+++ Pac.py
@@ -1,17 +1,18 @@
 import pygame
+import numpy as np
+from numpy import array
 
 class Pac():
     def __init__(self, game, maze) -> None:
         self.game = game
         self.screen: pygame.Surface = game.screen
-        self.maze = maze
-        self.row: int = 0
-        self.col: int = 0
-        self.direction = (1, 0)
+        self.maze = np.array(maze)
+        self.pos = np.array((0, 0))
+        self.direction = np.array((0, 1))
         self.move_timer = 0
         self.move_delay = 360
         self.speed: float = 0.5
-        self.rect = pygame.Rect(self.row, self.col, 50, 50)
+        self.rect = pygame.Rect(*self.pos, 50, 50)
         self.create()
 
     def create(self) -> None:
@@ -20,10 +21,10 @@ class Pac():
     def destroy(self):
         ...
 
-    def can_move(self, dire):
-        if not 0 <= self.row < len(self.maze[0]) or not 0 <= self.col < len(self.maze):
+    def can_move(self, dire: tuple):
+        if np.any(self.pos < 0) or np.any(self.pos >= self.maze.shape):
             return False
-        cell = self.maze[self.row][self.col]
+        cell = self.maze[*self.pos]
         walls = format(int(cell, 16), '04b')
         if dire == (-1, 0):
             return walls[3] == "0"
@@ -35,7 +36,7 @@ class Pac():
             return walls[2] == "0"
 
     def update(self, keys, dt):
-        new_dir = self.direction
+        new_dir: tuple = tuple(self.direction)
         if keys[pygame.K_LEFT]:
             new_dir = (0, -1)
         if keys[pygame.K_RIGHT]:
@@ -46,12 +47,14 @@ class Pac():
             new_dir = (1, 0)
 
         self.move_timer += dt
-        if self.can_move(new_dir) and self.move_timer >= self.move_delay:
+        if self.move_timer >= self.move_delay:
+            if self.can_move(new_dir):
                 self.move_timer = 0
-                self.direction = new_dir
-                self.row += self.direction[0]
-                self.col += self.direction[1]
+                self.direction = np.array(new_dir)
+                self.pos += self.direction
 
         blocksize = self.game.WINDOW_WIDTH // self.game.conf.width
-        self.rect = pygame.Rect(self.col * blocksize, self.row * blocksize, 50, 50)
+        self.rect = pygame.Rect(self.pos[1] * blocksize,
+                                self.pos[0] * blocksize,
+                                50, 50)
         _ = pygame.draw.rect(self.screen, (220, 200, 0), self.rect, 10)
