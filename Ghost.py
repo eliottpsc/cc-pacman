e5d1221b1c5f0ba536b0e178f262961a269c36f4
lgrosse <logan.grosse@learner.42.tech>
2026-07-14 09:40:11 +0200

    feat: add random movement for ghost

--- Ghost.py
+++ Ghost.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pygame
 import numpy as np
+import random as rd
 from Entity import Entity
 
 
@@ -11,6 +12,18 @@ class Ghost(Entity):
         self.rect = pygame.Rect(*self.pos, 50, 50)
 
     def update(self, dt):
+        new_dir = rd.choice(((0, -1), (0, 1), (-1, 0), (1, 0)))
+        self.move_timer += dt
+        if self.move_timer >= self.move_delay:
+            if self.can_move(new_dir):
+                self.move_timer = 0
+                self.direction = np.array(new_dir)
+                self.pos += self.direction
+
+        blocksize = self.game.WINDOW_WIDTH // self.game.conf.width
+        self.rect = pygame.Rect(self.pos[1] * blocksize,
+                                self.pos[0] * blocksize,
+                                50, 50)
         pygame.draw.rect(self.screen, (220, 0, 200), self.rect, 10)
 
 
