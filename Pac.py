fee18cef62605f8408ef76c13a0b3eb895a66023
logan grosse <logan.grosse@learner.42.tech>
2026-07-16 16:15:47 +0200

    feat: replace rect with rotating sprite

--- Pac.py
+++ Pac.py
@@ -6,9 +6,13 @@ from Entity import Entity
 class Pac(Entity):
     def __init__(self, game, maze) -> None:
         super().__init__(game, maze)
+        self.image = pygame.image.load("assets/pac.png")
+        self.image = pygame.transform.scale(self.image, (50, 50))
 
     def update(self, keys, dt):
-        new_dir= tuple(self.direction)
+        dire = tuple(self.direction.tolist())
+        new_dir = dire
+        image = self.image
         if keys[pygame.K_LEFT]:
             new_dir = (0, -1)
         if keys[pygame.K_RIGHT]:
@@ -22,13 +26,19 @@ class Pac(Entity):
 
         self.move_timer += dt
         if self.move_timer >= self.move_delay:
+            self.move_timer = 0
             if self.can_move(new_dir):
-                self.move_timer = 0
                 self.direction = np.array(new_dir)
                 self.pos += self.direction
+            elif self.can_move(dire):
+                self.pos += self.direction
+
+        rot = {(0, -1): 0, (0, 1): 180, (-1, 0): 270, (1, 0): 90}
+        image = pygame.transform.rotate(self.image, rot[dire])
 
         blocksize = self.game.WINDOW_WIDTH // self.game.conf.width
         self.rect = pygame.Rect(self.pos[1] * blocksize,
                                 self.pos[0] * blocksize,
                                 50, 50)
-        pygame.draw.rect(self.screen, (220, 200, 0), self.rect, 10)
+        self.screen.blit(image, self.rect)
+#       pygame.draw.rect(self.screen, (220, 200, 0), self.rect, 10)
